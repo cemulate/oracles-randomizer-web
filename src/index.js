12c6f3915e417e6b7333404a9927e93a403e2bec
Chase Meadors <c.ed.mead@gmail.com>
2024-06-20 22:11:01 -0600

    Add plausible

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './styles/theme.scss';
 import Vue from 'vue';
 import App from './components/App.vue';
 
+import Plausible from 'plausible-tracker';
+Plausible({
+    domain: 'cemulate.github.io/oracles-randomizer-web',
+    apiHost: 'https://plausible.351321.xyz',
+}).enableAutoPageviews();
+
 const body = document.getElementsByTagName('body')[0];
 const app = document.createElement('div');
 app.setAttribute('id', 'app');
